08c1f40f9b5c7c49d53f905a4e968a56f50f4743
KimISeob <seobew@gmail.com>
2017-09-07 14:00:42 +0900

    css and seed part of product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+
+10.times do |i|
+  Product.create(
+    title: "Programming Ruby 1.#{i}",
+    description:
+      %{<p>
+        Ruby is the fastest growing and most exciting dynamic language out there.
+        If you need to get working programs delivered fast, you should add Ruby to your toolbox.
+        </p>},
+    image_url: 'ruby.jpg',
+    price: 49.95
+  )
+end
+
+=begin
+Product.create(
+  title: 'Programming Ruby 1.9',
+  description:
+    %{<p>
+      Ruby is the fastest growing and most exciting dynamic language out there.
+      If you need to get working programs delivered fast, you should add Ruby to your toolbox.
+      </p>},
+  image_url: 'ruby.jpg',
+  price: 49.95
+)
+=end

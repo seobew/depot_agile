7a5d0d8044f04f84147049ea5e7023d083f4ae23
KimISeob <seobew@gmail.com>
2017-09-19 13:15:29 +0900

    handling line_items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiceRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+    end
 end

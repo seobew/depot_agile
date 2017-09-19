08206b7f26537270e7429eb6920af753c9a5317a
KimISeob <seobew@gmail.com>
2017-09-19 15:49:03 +0900

    add add_product method to cart model

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,7 +27,8 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     product = Product.find(params[:product_id])
     #@line_item = LineItem.new(line_item_params)
-    @line_item = @cart.line_items.build(product:product)
+   # @line_item = @cart.line_items.build(product:product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
---
+++ db/migrate/20170919060021_combine_itmes_to_cart.rb
@@ -0,0 +1,22 @@
+class CombineItmesToCart < ActiveRecord::Migration
+  def up
+    Cart.all.each do |cart|
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+
+      sums.each do |product_id, quantity|
+        cart.line_items.where(product_id: product_id).delete_all
+
+        cart.line_items.create(product_id: product_id, quantity: quantity)
+      end
+    end
+  end
+
+  def down
+    LineItem.where("quantity>1").each do |line_item|
+      line_item.quantity.times do
+        LineItem.create cart_id: line_item.cart_id, product_id: line_item.product_id, quantity:1
+      end
+      line_item.destroy
+    end
+  end
+end
